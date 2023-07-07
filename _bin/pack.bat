e4b9425fc1035e3b20e577de9da61c86e2332d4e
abevol <abevol@pm.me>
2023-07-07 08:49:44 +0800

    Fix package file missing error.

--- _bin/pack.bat
+++ _bin/pack.bat
@@ -6,19 +6,19 @@ if not exist ".\packages\" (
     MKDIR ".\packages\"
 )
 
-XCOPY /Y "..\BetterPayableUpgrade\bin\Release\net6.0" ".\packages\KingdomMod.BetterPayableUpgrade\"
+XCOPY /Y /S "..\BetterPayableUpgrade\bin\Release\net6.0" ".\packages\KingdomMod.BetterPayableUpgrade\"
 set filename=".\packages\KingdomMod.BetterPayableUpgrade_v%version%.zip"
 7za a -tzip "%filename%" ".\packages\KingdomMod.BetterPayableUpgrade"
 
-XCOPY /Y "..\DevTools\bin\Release\net6.0" ".\packages\KingdomMod.DevTools\"
+XCOPY /Y /S "..\DevTools\bin\Release\net6.0" ".\packages\KingdomMod.DevTools\"
 set filename=".\packages\KingdomMod.DevTools_v%version%.zip"
 7za a -tzip "%filename%" ".\packages\KingdomMod.DevTools"
 
-XCOPY /Y "..\OverlayMap\bin\Release\net6.0" ".\packages\KingdomMod.OverlayMap\"
+XCOPY /Y /S "..\OverlayMap\bin\Release\net6.0" ".\packages\KingdomMod.OverlayMap\"
 set filename=".\packages\KingdomMod.OverlayMap_v%version%.zip"
 7za a -tzip "%filename%" ".\packages\KingdomMod.OverlayMap"
 
-XCOPY /Y "..\StaminaBar\bin\Release\net6.0" ".\packages\KingdomMod.StaminaBar\"
+XCOPY /Y /S "..\StaminaBar\bin\Release\net6.0" ".\packages\KingdomMod.StaminaBar\"
 set filename=".\packages\KingdomMod.StaminaBar_v%version%.zip"
 7za a -tzip "%filename%" ".\packages\KingdomMod.StaminaBar"
 
